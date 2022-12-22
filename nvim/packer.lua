3734f54d01d4a16684558c327b1337f48ba9f977
Ole Christian Enger <ole.enger@gmail.com>
2022-12-22 10:57:26 +0000

    test

---
+++ nvim/packer.lua
@@ -0,0 +1,42 @@
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
+end
+
+local packer_bootstrap = ensure_packer()
+
+return require('packer').startup(function(use)
+  use 'wbthomason/packer.nvim'
+
+  use({
+	  "neanias/everforest-nvim",
+	  -- Optional; default configuration will be used if setup isn't called.
+	  config = function()
+		  require("everforest").setup()
+	  end,
+  })
+
+    use('theprimeagen/harpoon')
+  
+
+    -- Automatically set up your configuration after cloning packer.nvim
+  -- Put this at the end after all plugins
+
+  if packer_bootstrap then
+	  require('packer').sync()
+
+	  require("everforest").setup({
+		  background = "hard",
+	  })
+	 
+
+  end
+
+  
+  end)
