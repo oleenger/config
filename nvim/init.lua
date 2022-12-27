bb5cf17a3389f31fd1ee37bd25e1b63d97eeb2c4
Ole Christian Enger <ole.enger@gmail.com>
2022-12-27 19:27:54 +0000

    lualine

--- nvim/init.lua
+++ nvim/init.lua
@@ -17,7 +17,6 @@ return require('packer').startup(function(use)
     use({"neanias/everforest-nvim", config = function() require("everforest").setup() end, })
     use('theprimeagen/harpoon')
     use { 'nvim-telescope/telescope.nvim', tag = '0.1.0', requires = { {'nvim-lua/plenary.nvim'} } }
-    use 'nvim-lualine/lualine.nvim' -- Fancier statusline
     use 'lukas-reineke/indent-blankline.nvim' -- Add indentation guides even on blank lines
     use 'tpope/vim-sleuth' -- Detect tabstop and shiftwidth automatically
     use 'nanozuki/tabby.nvim'
@@ -56,10 +55,12 @@ return require('packer').startup(function(use)
 
     use {
         'nvim-tree/nvim-tree.lua',
-        requires = {
-            'nvim-tree/nvim-web-devicons', -- optional, for file icons
-        },
         tag = 'nightly' -- optional, updated every week. (see issue #1193)
+    }
+    
+    use {
+      'nvim-lualine/lualine.nvim',
+      requires = { 'kyazdani42/nvim-web-devicons', opt = true }
     }
 
     if packer_bootstrap then
@@ -67,24 +68,17 @@ return require('packer').startup(function(use)
 
         require("everforest").setup({ background = "hard" })
 
-        require("lualine").setup({
-            options = {
-                theme = "auto", -- Can also be "auto" to detect automatically.
-            }
-        })
 
         require('indent_blankline').setup {
             char = '┊',
             show_trailing_blankline_indent = false,
         }
 
-
         require("nvim-autopairs").setup({
             disable_filetype = { "TelescopePrompt" },
         })
         require("colorizer").setup()
         require("gitsigns").setup()
-
 
     end
 
@@ -95,6 +89,7 @@ return require('packer').startup(function(use)
     require('plugins.telescope')
     require('plugins.tabby')
     require('plugins.treesitter')
+    require('plugins.lualine')
 
 end)
 
