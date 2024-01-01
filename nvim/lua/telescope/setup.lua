5c5ca99a22470764d352fb26bc25f91a63f9c233
oleenger <ole.enger@gmail.com>
2024-01-01 13:39:03 +0100

    lazy

---
+++ nvim/lua/telescope/setup.lua
@@ -0,0 +1,36 @@
+require("telescope").setup({
+  extensions = {
+    lazy = {
+      -- Optional theme (the extension doesn't set a default theme)
+      theme = "ivy",
+      -- Whether or not to show the icon in the first column
+      show_icon = true,
+      -- Mappings for the actions
+      mappings = {
+        open_in_browser = "<C-o>",
+        open_in_file_browser = "<M-b>",
+        open_in_find_files = "<C-f>",
+        open_in_live_grep = "<leader>a",
+        open_in_terminal = "<C-t>",
+        open_plugins_picker = "<C-b>", -- Works only after having called first another action
+        open_lazy_root_find_files = "<C-r>f",
+        open_lazy_root_live_grep = "<C-r>g",
+        change_cwd_to_plugin = "<C-c>d",
+      },
+      -- Configuration that will be passed to the window that hosts the terminal
+      -- For more configuration options check 'nvim_open_win()'
+      terminal_opts = {
+        relative = "editor",
+        style = "minimal",
+        border = "rounded",
+        title = "Telescope lazy",
+        title_pos = "center",
+        width = 0.5,
+        height = 0.5,
+      },
+      -- Other telescope configuration options
+    },
+  },
+})
+
+-- require("telescope").load_extension "lazy"
