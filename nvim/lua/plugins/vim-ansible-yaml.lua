b55ef0213ca2180723a392dc93c2837811a21707
oleenger <ole.enger@gmail.com>
2024-01-07 10:28:41 +0100

    highlighting yaml

--- nvim/lua/plugins/vim-ansible-yaml.lua
+++ nvim/lua/plugins/vim-ansible-yaml.lua
@@ -2,3 +2,11 @@ vim.api.nvim_create_autocmd("BufWinEnter", {
         pattern = "*.yml",
         command = "set filetype=ansible",
 })
+
+
+vim.cmd[[
+        highlight PreProc guifg=orange
+        highlight Boolean guifg=darkorange
+        highlight ansibleRepeat guifg=darkyellow
+]]
+
