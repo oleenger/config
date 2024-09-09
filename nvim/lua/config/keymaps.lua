11c7f68c0a128e311b55eeb5638941987646ba72
oleenger <ole.enger@gmail.com>
2024-09-09 10:19:11 +0200

    telescope

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -4,6 +4,5 @@ map("n", "<C-p>", ":!~/pushdbbrowser.sh<cr><cr>")
 
 map("n", "<leader>sn", ":Telescope live_grep search_dirs=~/.nb/home/<cr>")
 
-#NB
 map("n", "<leader>nb", ":!nb ")
 map("n", "<leader>nn", ":e /home/oleenger/.nb/home/")
