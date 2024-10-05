895e2081b3419eae4ac9b14e9bfdb086221cec0f
oleenger <ole.enger@gmail.com>
2024-10-05 21:33:12 +0200

    pac

--- ssh/generate_ssh_config.py
+++ ssh/generate_ssh_config.py
@@ -1,7 +1,11 @@
 import yaml
 import pprint as pprint
 
+START_PORT = 1200
+
 def generate(content):
+  dynamic_forward = START_PORT
+  
   output = f"""
     ServerAliveInterval { content['ServerAliveInterval'] }
     ServerAliveCountMax { content['ServerAliveCountMax'] }
@@ -13,19 +17,18 @@ def generate(content):
     item = content['hostnames'][host]
 
     for hostname in content['hostnames'][host]['hosts']:
+
       item_str = f"""
         Host { hostname }
         HostName { hostname }.{ host }
         IdentityFile { content['IdentityFile'] }
+        DynamicForward { str(dynamic_forward) }
       """
       
+      dynamic_forward = dynamic_forward+1
+
       if not "NoProxyJump" in item.keys():
         item_str += f"\tProxyJump { content['ProxyJumpDefault'] }\n"
-
-      if "DynamicForward" in item.keys():
-        item_str += f"\tDynamicForward { item['DynamicForward'] }\n"
-     
-      # if hostname in content['hostnames'][host].keys()
 
       if item['hosts'][hostname] != None and "Port" in item['hosts'][hostname].keys():
           item_str += f"\tPort { item['hosts'][hostname]['Port'] }\n"
@@ -46,3 +49,58 @@ with open("hosts.yml") as stream:
     generate(content)
   except yaml.YAMLError as exc:
     print(exc)
+
+
+
+
+hosts = list()
+with open("config") as file:
+  for line in file:
+    if "Host" in line:
+      obj = dict()
+    if "HostName" in line:
+      hostname = line.split("HostName ")[1].strip()
+      obj['hostname'] = hostname
+      print(hostname)
+    if "DynamicForward" in line:
+      dynamic_forward = line.split("DynamicForward")[1].strip()
+      print(dynamic_forward)
+      obj['dynamic_forward'] = dynamic_forward
+      hosts.append(obj)
+
+pac = """
+function FindProxyForURL(url, requestedHost) {
+    "use strict";
+    alert("Trying url = " + url + " *** host = " + host + " *** Resolved IP = " + dnsResolve(host));
+    var hosts =
+    {
+"""
+
+for host in hosts:
+  pac += f"'{ host['hostname'] }': { str(host['dynamic_forward']) },\n"
+
+#VIPs
+with open("hosts.yml") as stream:
+  content = yaml.safe_load(stream)
+
+  for url in content["vips"]:
+    pac += f"'{ url }': { START_PORT },\n"
+
+pac += """
+   };
+  for (var host in hosts) {
+    var port = hosts[host];
+    if (requestedHost == host || shExpMatch(requestedHost, host)) {
+      return 'SOCKS5 localhost:' + port;
+    }
+  }
+  return 'DIRECT';
+}
+"""
+
+
+print(pac)
+
+f = open("/mnt/c/Users/t927604/proxy.pac", "w")
+f.write(pac)
+f.close()
