2d58f40c6950511c88c722500abcda97761902a8
oleenger <ole.enger@gmail.com>
2024-11-11 20:32:29 +0100

    keymaps

--- nvim/lua/plugins/telescope-file-browser.lua
+++ nvim/lua/plugins/telescope-file-browser.lua
@@ -6,4 +6,10 @@ return {
         }
     },
     dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
+
+    config = function()
+        -- Open Telescope File Browser
+        vim.keymap.set("n", "<space>fb", ":Telescope file_browser path=%:p:h select_buffer=true<CR>")
+
+    end
 }
