f730d256d0b61669b92f1ab97b9f73940a95c5cd
oleenger <ole.enger@gmail.com>
2024-11-11 22:26:34 +0100

    fugitive

--- nvim/lua/plugins/fugitive.lua
+++ nvim/lua/plugins/fugitive.lua
@@ -6,7 +6,8 @@ return {
     vim.keymap.set("n", "gs", ":Git status<cr>", { desc = "Git status" })
     vim.keymap.set("n", "gd", ":Git diff<cr>", { desc = "Git diff" })
     vim.keymap.set("n", "ga", ":Git add .<cr>", { desc = "Git add ." })
-    vim.keymap.set("n", "gp", ":Git commit<cr>", { desc = "Git commit" })
+    vim.keymap.set("n", "gc", ":Git commit<cr>", { desc = "Git commit" })
+    vim.keymap.set("n", "gp", ":Git push<cr>", { desc = "Git push" })
       
 
 
