80f72351a03c1bcbe08c3b201d2c19e817c0bd7f
Ole Christian Enger <oleenger@users.noreply.github.com>
2026-03-04 22:01:59 +0100

    Feat/data assets (#8)
    * div
    
    * tmux
    
    * tmux bar
    
    * tmux + skills

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -26,7 +26,6 @@ return {
       },
       format_on_save = {
         lsp_fallback = true,
-        async = false,
         timeout_ms = 1000,
       },
     }
--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -30,7 +30,7 @@ return { -- Highlight, edit, and navigate code
       'html',
     },
     -- Autoinstall languages that are not installed
-    auto_install = true,
+    auto_install = false,
     highlight = {
       enable = true,
       -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
